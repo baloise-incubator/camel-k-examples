15819b296ea16b3d8517fc1189948784394e1c2a
David Weber <david.weber@w3tec.ch>
2021-11-04 13:45:35 +0100

    feat(camel-k): add openapi example with jar

--- src/main/java/ch/baloise/example/openapi/OpenApiRoute.java
+++ src/main/java/ch/baloise/example/openapi/OpenApiRoute.java
@@ -4,28 +4,22 @@
 // camel-k: dependency=camel-jackson
 // camel-k: dependency=camel-openapi-java
 // camel-k: dependency=mvn:org.projectlombok:lombok:1.18.22
+// camel-k: resource=target/camel-k-examples-1.0-SNAPSHOT.jar
+// camel-k: trait=jvm.classpath=/etc/camel/resources/camel-k-examples-1.0-SNAPSHOT.jar
 
 package ch.baloise.example.openapi;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.camel.BindToRegistry;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.apache.camel.model.rest.RestParamType;
-
-import javax.validation.constraints.NotNull;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Random;
-import java.util.TreeMap;
 
 @Slf4j
 public class OpenApiRoute extends RouteBuilder {
     @Override
     public void configure() {
+        getContext().getRegistry().bind("userService", new UserService());
+
         restConfiguration()
                 .component("platform-http")
                 .bindingMode(RestBindingMode.json)
@@ -75,49 +69,5 @@ public class OpenApiRoute extends RouteBuilder {
                 .param().name("did").type(RestParamType.path).description("The id of the department to get").dataType("integer").required(true).endParam()
                 .responseMessage().code(200).message("All users").endResponseMessage()
                 .to("bean:userService?method=listUsers");
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    public static class User {
-        private int id;
-        @NotNull
-        private String name;
-    }
-
-    @BindToRegistry("userService")
-    @Slf4j
-    public static class UserService {
-        private final Map<String, User> users = new TreeMap<>();
-        private Random ran = new Random();
-        public UserService() {
-            users.put("123", new User(123, "John Doe"));
-            users.put("456", new User(456, "Donald Duck"));
-            users.put("789", new User(789, "Slow Turtle"));
-        }
-
-        public User getUser(String id) {
-            log.info("getUser", id);
-            if ("789".equals(id)) {
-                int delay = 500 + ran.nextInt(1500);
-                try {
-                    Thread.sleep(delay);
-                } catch (Exception e) {
-                    // ignore
-                }
-            }
-            return users.get(id);
-        }
-
-        public Collection<User> listUsers() {
-            log.info("listUsers");
-            return users.values();
-        }
-
-        public void updateUser(User user) {
-            log.info("updateUser", user);
-            users.put("" + user.getId(), user);
-        }
     }
 }
---
+++ src/main/java/ch/baloise/example/openapi/User.java
@@ -0,0 +1,16 @@
+package ch.baloise.example.openapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private int id;
+    @NotNull
+    private String name;
+}
