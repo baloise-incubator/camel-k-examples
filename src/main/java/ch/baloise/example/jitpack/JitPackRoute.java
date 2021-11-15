b849e21f4bc5f5ff154a8a8a162aeccc504956aa
Gero Hoffmann <gero.hoffmann@baloise.ch>
2021-11-15 18:32:09 +0100

    added Slf4j for jitpack route

--- src/main/java/ch/baloise/example/jitpack/JitPackRoute.java
+++ src/main/java/ch/baloise/example/jitpack/JitPackRoute.java
@@ -1,10 +1,12 @@
 package ch.baloise.example.jitpack;
 
 import ch.baloise.example.jitpack.subpackage.MyProcessor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
 
 //  kamel run -d github:baloise-incubator/camel-k-examples/main-SNAPSHOT ./src/main/java/ch/baloise/example/jitpack/JitPackRoute.java
 
+@Slf4j
 public class JitPackRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
