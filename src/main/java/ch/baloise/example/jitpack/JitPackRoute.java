ac88afc09a7017243998cfb0504d9512f5ba0fa3
Gero Hoffmann <gero.hoffmann@baloise.ch>
2021-11-15 19:40:55 +0100

    cleanup JitPackRoute

--- src/main/java/ch/baloise/example/jitpack/JitPackRoute.java
+++ src/main/java/ch/baloise/example/jitpack/JitPackRoute.java
@@ -1,12 +1,11 @@
+// camel-k: name=jit-pack-route
+// camel-k: dependency=github:baloise-incubator/camel-k-examples/84261f26b0f7f5d9fe9453ada8997af2a0df6884
+
 package ch.baloise.example.jitpack;
 
 import ch.baloise.example.jitpack.subpackage.MyProcessor;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
 
-//  kamel run -d github:baloise-incubator/camel-k-examples/main-SNAPSHOT ./src/main/java/ch/baloise/example/jitpack/JitPackRoute.java
-
-@Slf4j
 public class JitPackRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
